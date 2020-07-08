88d9900baf77436018f0d3df00021fb02ba060e9
Nan Yu <nanyu@google.com>
2020-07-08 10:43:28 -0700

    Update region from us-east-2 to us-west-2

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "us-west-2"
   description = "AWS region"
 }
 
 provider "aws" {
   version = ">= 2.28.1"
-  region  = "us-east-2"
+  region  = "us-west-2"
 }
 
 data "aws_availability_zones" "available" {}
